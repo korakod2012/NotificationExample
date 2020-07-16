48fdd9a430f2453f30469310118a9ae46751e2ea
blackip <kkoodd2012@gmail.com>
2020-07-16 11:34:20 +0700

    Initial commit

---
+++ app/src/main/java/com/example/notificationexample/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.notificationexample
+
+import android.app.*
+import android.content.Context
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.RemoteViews
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var notificationManager: NotificationManager
+    lateinit var notificationChannel: NotificationChannel
+    lateinit var builder: Notification.Builder
+    private val channelId = "com.example.notificationexample"
+    private val description = "Test notification"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        btn_notify.setOnClickListener {
+
+            val intent = Intent(this, LauncherActivity::class.java)
+            val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+            val contentView = RemoteViews(packageName, R.layout.notification_layout)
+            contentView.setTextViewText(R.id.tv_title, "Reminder : time to workout ")
+            contentView.setTextViewText(R.id.tv_content, "This is exercise time !!")
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                notificationChannel = NotificationChannel(channelId, description, NotificationManager.IMPORTANCE_HIGH)
+                notificationChannel.enableLights(true)
+                notificationChannel.lightColor = Color.GREEN
+                notificationChannel.enableVibration(false)
+                notificationManager.createNotificationChannel(notificationChannel)
+
+                builder = Notification.Builder(this, channelId)
+//                        .setContentTitle("My Code Test Example")
+//                        .setContentText("Hello Dad")
+                        .setContent(contentView)
+                        .setSmallIcon(R.drawable.ic_launcher_round)
+                        .setLargeIcon(BitmapFactory.decodeResource(this.resources, R.drawable.ic_launcher))
+                        .setContentIntent(pendingIntent)
+            }else{
+                builder = Notification.Builder(this)
+//                        .setContentTitle("My Code Test Example")
+//                        .setContentText("Hello Dad")
+                        .setContent(contentView)
+                        .setSmallIcon(R.drawable.ic_launcher_round)
+                        .setLargeIcon(BitmapFactory.decodeResource(this.resources, R.drawable.ic_launcher))
+                        .setContentIntent(pendingIntent)
+            }
+            notificationManager.notify(1234, builder.build())
+        }
+    }
+}
